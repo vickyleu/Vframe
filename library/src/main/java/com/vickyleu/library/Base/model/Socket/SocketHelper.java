b175cf73a551b5896f2677fdcc290cb92def01e5
Vickyleu <yu6564172@gmail.com>
2016-05-18 21:58:02 +0800

    change

--- library/src/main/java/com/vickyleu/library/Base/model/Socket/SocketHelper.java
+++ library/src/main/java/com/vickyleu/library/Base/model/Socket/SocketHelper.java
@@ -1,74 +1,94 @@
-//package com.vickyleu.library.Base.model.Socket;
-//
-//import android.os.AsyncTask;
-//
-//import java.io.IOException;
-//import java.net.InetSocketAddress;
-//import java.net.ServerSocket;
-//import java.net.Socket;
-//import java.net.SocketAddress;
-//import java.util.concurrent.Callable;
-//import java.util.concurrent.ExecutorService;
-//import java.util.concurrent.Executors;
-//import java.util.concurrent.Future;
-//
-///**
-// * Created by Administrator on 2016/5/17.
-// */
-//public class SocketHelper {
-//    private static ExecutorService exe;
-//
-//    public static Future<Socket> Connect(String iNet, int port) {
-//
-//        if (exe != null && !exe.isShutdown()) exe.shutdown();
-//        exe = Executors.newSingleThreadExecutor();
-//        Future<Socket> result = exe.submit(new Task(iNet, port));
-//
-//
-//
-//        return result;
-//    }
-//
-//
-//    public static Future<Integer> Server() {
-//
-//        if (exe != null && !exe.isShutdown()) exe.shutdown();
-//        exe = Executors.newSingleThreadExecutor();
-//        Future<Integer> result = exe.submit(new Task());
-//
-////        result.
-//
-//        Socket mSocket;
-//        try {
-//            ServerSocket ss = new ServerSocket();
-//            mSocket = ss.accept();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//            return null;
-//        }
-//        return result;
-//    }
-//
-//    private static class Task implements Callable<Socket> {
-//        String iNet;
-//        int port;
-//
-//        public Task(String iNet, int port) {
-//            this.iNet = iNet;
-//            this.port = port;
-//        }
-//
-//        @Override
-//        public Socket call() throws Exception {
-//            Socket mSocket = new Socket();
-//            SocketAddress addr = new InetSocketAddress(iNet, port);
-//            try {
-//                mSocket.connect(addr, 3000);
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//                return null;
-//            }
-//            return mSocket;
-//        }
-//    }
-//}
+package com.vickyleu.library.Base.model.Socket;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+/**
+ * Created by Administrator on 2016/5/17.
+ */
+public class SocketHelper {
+    private static ExecutorService exe;
+    static ServerSocket ss;
+
+    public static Future<Socket> Connect(String iNet, int port) {
+
+        if (exe != null && !exe.isShutdown()) exe.shutdown();
+        exe = Executors.newSingleThreadExecutor();
+        Future<Socket> result = exe.submit(new ClientTask(iNet, port));
+        return result;
+    }
+
+    public static void ReleaseSocket() {
+        if (ss != null) try {
+            ss.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected static Future<Socket> Server(int port) {
+
+        if (exe != null && !exe.isShutdown()) exe.shutdown();
+        exe = Executors.newSingleThreadExecutor();
+        if (ss == null) {
+            try {
+                ss = new ServerSocket();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        Future<Socket> result = exe.submit(new ServerTask(port));
+        return result;
+    }
+
+    private static class ClientTask implements Callable<Socket> {
+        String iNet;
+        int port;
+
+        public ClientTask(String iNet, int port) {
+            this.iNet = iNet;
+            this.port = port;
+        }
+
+        @Override
+        public Socket call() throws Exception {
+            Socket mSocket = new Socket();
+            try {
+                mSocket.connect(new InetSocketAddress(iNet, port), 3000);
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+            return mSocket;
+        }
+    }
+
+    private static class ServerTask implements Callable<Socket> {
+        int port;
+
+
+        public ServerTask(int port) {
+            this.port = port;
+        }
+
+        @Override
+        public Socket call() throws Exception {
+            Socket mSocket = null;
+            try {
+
+                if (!ss.isBound()) ss.bind(new InetSocketAddress(port));
+                mSocket = ss.accept();
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+            return mSocket;
+        }
+    }
+}
